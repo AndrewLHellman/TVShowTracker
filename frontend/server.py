239a9885b45c01d152db68df9b3bfd01c3eab0db
AndrewLHellman <alhdnb@umsystem.edu>
2023-11-04 15:53:18 -0500

    Home page

--- frontend/server.py
+++ frontend/server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template("index.html")
+
+@app.route("/tvshows")
+def tvshows():
     return render_template("tvshows.html")
 
 if __name__ == "__main__":
