9a385b54869a9b1f6bcce3b99d41980ac108c2f6
AndrewLHellman <alhdnb@umsystem.edu>
2023-11-04 19:18:13 -0500

    Adding tvdb interaction

--- frontend/static/scripts/main.js
+++ frontend/static/scripts/main.js
@@ -3,14 +3,14 @@ const mainContainer = document.querySelector("main");
 
 let containerNum = 1;
 const arrestedDevelopment = {
-  image: "images/ArrestedDevelopment.jpg",
+  image: "https://image.tmdb.org/t/p/w500/qMzwO952hMWQSCfHkp7IL20s4K7.jpg",
   description: "Arrested Development is an American television sitcom created by Mitchell Hurwitz. It aired on Fox for three seasons from November 2, 2003, to February 10, 2006, followed by two seasons on Netflix, season four being released in 2013 and season five being released in 2018 and 2019.",
   genre: "Comedy",
   recommenders: ["Tessa", "Andrew", "LeeAnn", "Jenaya", "Jeffrey"]
 }
 
 const kimsConvenience = {
-    image: "images/KimsConvenience.jpg",
+    image: "https://image.tmdb.org/t/p/w500/82Vue6MPuYZzlpggR65cvWdBdy.jpg",
     description: "Kim's Convenience (Korean: 김씨네 편의점; Hanja: 金氏네 便宜店; RR: Gimssine Pyeonuijeon) is a Canadian television sitcom that aired on CBC Television from October 2016 to April 2021. It depicts the Korean Canadian Kim family that runs a convenience store in the Moss Park neighbourhood of Toronto: parents \"Appa\" (Paul Sun-Hyung Lee) and \"Umma\" (Jean Yoon) – Korean for dad and mom, respectively – along with their daughter Janet (Andrea Bang) and estranged son Jung (Simu Liu). Other characters include Jung's friend and coworker Kimchee (Andrew Phung), his manager Shannon (Nicole Power) and Janet's friend Gerald Tremblay (Ben Beauchemin). The series is based on Ins Choi's 2011 play of the same name.",
     genre: "Comedy",
     recommenders: ["Andrew"]
@@ -47,7 +47,7 @@ function createShowRec (show) {
     let showImage = document.createElement("img");
     showImage.className = "showImage";
     showImage.id = `showImg-${containerNum}`;
-    showImage.src = `http://34.170.130.159:5000/images?image_file_name=${show["image"]}`;
+    showImage.src = show[image];
     showContainer.appendChild(showImage);
 
     let showText = document.createElement("div");
